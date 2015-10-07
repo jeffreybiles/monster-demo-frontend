3a50176039967a024465ab578aac6bbcc672e0c0
jeffreybiles <bilesjeffrey@gmail.com>
2015-10-07 04:27:35 -0500

    small changes to prep for next screencast

--- app/services/session.js
+++ app/services/session.js
@@ -2,15 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   currentUser: null,
-  store: Ember.inject.service(),
   login(user){
-    this.set('currentUser', user)
+    this.set("currentUser", user)
   },
   logout(){
-    this.set('currentUser', null)
-  },
-  tempFirstUser: function(){
-    // only for a bit, setting currentUser as first user
-    this.set('currentUser', this.get('store').find('user', 1))
-  }.on('init')
+    this.set("currentUser", null)
+  }
 });
