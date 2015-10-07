4289cf1699a9ec45a5a47af64c37f2782f9eb031
jeffreybiles <bilesjeffrey@gmail.com>
2015-10-07 06:01:47 -0500

    Remember sessions with cookies

--- app/services/session.js
+++ app/services/session.js
@@ -2,10 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   currentUser: null,
+  store: Ember.inject.service(),
   login(user){
     this.set("currentUser", user)
+    Cookies.set('userId', user.id)
   },
   logout(){
     this.set("currentUser", null)
-  }
+    Cookies.remove('userId')
+  },
+  initializeFromCookie: function(){
+    var userId = Cookies.get('userId');
+    if(!!userId){
+      var user = this.get('store').find('user', userId)
+      this.set('currentUser', user)
+    }
+  }.on('init')
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,6 +10,7 @@ module.exports = function(defaults) {
     }
   });
 
+  app.import('bower_components/js-cookie/src/js.cookie.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
