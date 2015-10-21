432890483add0085cfeea6db5fb90a93fb256052
jeffreybiles <bilesjeffrey@gmail.com>
2015-10-20 21:04:31 -0500

    flash and validations

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,14 +1,27 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations, {
+  showErrors: false,
   session: Ember.inject.service(),
+  validations: {
+    userName: {
+      presence: true
+    },
+    password: {
+      presence: true,
+      length: {minimum: 6}
+    }
+  },
   actions: {
     login(){
       let {userName, password} = this.getProperties('userName', 'password');
       this.get("session").login(userName, password).then(()=>{
+        this.get('flashMessages').success('You have signed in successfully')
         this.transitionToPreviousRoute()
       }).catch((reason)=>{
-        console.log("Error: ", reason)
+        this.set("showErrors", true)
+        this.get('flashMessages').danger(reason)
       })
     }
   },
