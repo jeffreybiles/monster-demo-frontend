30e3f969b655954abfe3e34e4a8cef3ca6ebd52f
jeffreybiles <bilesjeffrey@gmail.com>
2015-10-20 19:13:13 -0500

    login page

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,16 +3,25 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
   actions: {
-    login(user){
-      this.get("session").login(user)
-      var previousTransition = this.get('previousTransition');
-      if (previousTransition) {
-        this.set('previousTransition', null);
-        previousTransition.retry();
-      } else {
-        // Default back to homepage
-        this.transitionToRoute('index');
-      }
+    login(){
+      let {userName, password} = this.getProperties('userName', 'password');
+      this.get("session").login(userName, password).then(()=>{
+        this.transitionToPreviousRoute()
+      }).catch((reason)=>{
+        console.log("Error: ", reason)
+      })
     }
-  }  
+  },
+
+  transitionToPreviousRoute(){
+    var previousTransition = this.get('previousTransition');
+    if (previousTransition) {
+      this.set('previousTransition', null);
+      previousTransition.retry();
+    } else {
+      // Default back to homepage
+      this.transitionToRoute('index');
+    }
+  }
+
 })
--- app/services/session.js
+++ app/services/session.js
@@ -4,9 +4,19 @@ export default Ember.Service.extend({
   currentUser: null,
   isLoggedIn: Ember.computed.bool('currentUser'),
   store: Ember.inject.service(),
-  login(user){
-    this.set("currentUser", user)
-    Cookies.set('userId', user.id)
+  login(userName, password){
+    return new Promise((resolve, reject)=>{
+      if(userName == 'emberscreencasts' && password == 'awesome'){
+        this.get('store').findAll('user').then((response)=>{
+          var user = response.get('firstObject')
+          this.set("currentUser", user)
+          Cookies.set('userId', user.id)
+          resolve()
+        })
+      } else {
+        reject('Username and password did not match')
+      }
+    })
   },
   logout(){
     this.set("currentUser", null)
