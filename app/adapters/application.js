f554b3baeb1ad56e804cbf5cc180b8f94465cbeb
jeffreybiles <bilesjeffrey@gmail.com>
2015-11-03 09:39:11 -0600

    this is where the options go...

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,5 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  "port": 4200,
-  // "host": "0.0.0.0",
-  "live-reload": true,
-  "proxy": "http://localhost:3000", //why the fuck is this not working?
-  "environment": "development"
+
 });
