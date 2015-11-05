0a40caf92a1198bd5cc8100540ba13fb1dedaf20
jeffreybiles <bilesjeffrey@gmail.com>
2015-11-05 14:25:06 -0600

    class helpers

---
+++ app/services/yen-converter.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+// This is just a converter from Dollars to Yen
+// Could generalize later
+export default Ember.Service.extend({
+  currentCurrency: 'Yen',
+  exchangeRate: 121.60,
+  isInDollars: Ember.computed.equal('currentCurrency', 'Dollars'),
+  exchange(dollarAmount){
+    if(this.get('isInDollars')){
+      return dollarAmount
+    } else {
+      return dollarAmount * this.get('exchangeRate')
+    }
+  },
+  switchCurrency(){
+    if(this.get('isInDollars')){
+      this.set('currentCurrency', 'Yen')
+    } else {
+      this.set('currentCurrency', 'Dollars')
+    }
+  },
+});
