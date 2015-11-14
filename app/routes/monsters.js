00cde5beed88924f47d483101756e01f9c3a953b
jeffreybiles <bilesjeffrey@gmail.com>
2015-11-14 08:48:38 -0600

    hash, worlds

--- app/routes/monsters.js
+++ app/routes/monsters.js
@@ -3,14 +3,15 @@ import Authenticated from '../mixins/authenticated-route';
 
 export default Ember.Route.extend(Authenticated, {
   model(){
-    return new Ember.RSVP.Promise((resolve, reject)=>{
-      this.store.findAll('monster').then((response)=>{
-        console.log('the response is', response)
-        resolve(response)
-      }).catch((reason)=>{
-        console.log('finding monsters failed', reason)
-        reject(reason)
-      })
+    return new Ember.RSVP.hash({
+        worlds: this.store.findAll('world'),
+        monsters: this.store.findAll('monster'),
+        taco: true
+    }).then((response)=>{
+      console.log('the response was', response)
+      return response
+    }).catch((reason)=>{
+      console.log("rejected", reason)
     })
   }
 });
