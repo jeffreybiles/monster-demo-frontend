e86885cfc5c2f911cc1a6ebcd32cb8057bed802f
jeffreybiles <bilesjeffrey@gmail.com>
2015-11-21 02:02:50 -0600

    ui-navigator-service

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
+  navigator: Ember.inject.service(),
   actions: {
     logout(){
       this.get("session").logout()
