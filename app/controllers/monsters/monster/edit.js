18702bc83c9316f5922a707e49e16f06cff8791a
jeffreybiles <bilesjeffrey@gmail.com>
2015-12-03 02:37:33 -0600

    Cancel Button and listing of changed properties

--- app/controllers/monsters/monster/edit.js
+++ app/controllers/monsters/monster/edit.js
@@ -3,15 +3,29 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save(){
-      this.get('model').save().then((monster)=>{
-        this.transitionToRoute('monsters.monster', monster)
+      this.get('model').save().then(()=>{
+        this.transitionToRoute('monsters.monster')
       })
     },
     destroy(){
       this.get('model').destroyRecord().then(()=>{
         this.transitionToRoute('monsters.monster')
       })
-
+    },
+    cancel(){
+      this.get('model').rollbackAttributes()
+      this.transitionToRoute('monsters.monster')
     }
-  }
+  },
+  changedAttributesHash: Ember.computed('model.{name,level,imageUrl,price}', function(){
+    let changeAttributes = this.get('model').changedAttributes()
+    let usableHash = Object.keys(changeAttributes).map(function(attributeName){
+      return {
+        key: attributeName,
+        earlierValue: changeAttributes[attributeName][0],
+        currentValue: changeAttributes[attributeName][1]
+      }
+    });
+    return usableHash
+  })
 })
