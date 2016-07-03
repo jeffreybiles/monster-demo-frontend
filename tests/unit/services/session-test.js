54b8aa89fe33e7b68d8227277555cf1e318f482c
jeffreybiles <bilesjeffrey@gmail.com>
2016-07-03 13:31:33 -0500

    `ember test` passes

--- tests/unit/services/session-test.js
+++ tests/unit/services/session-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:session', 'Unit | Service | session', {
   // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  needs: ['model:user']
 });
 
 // Replace this with your real tests.
